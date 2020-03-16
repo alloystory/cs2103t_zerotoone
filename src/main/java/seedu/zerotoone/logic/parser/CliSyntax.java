00273b19786f26727bcf104392ed119b1be75018
Aloysius Chan <alloystory@gmail.com>
2020-03-17 00:56:48 +0800

    add prefixes to CliSyntax

--- src/main/java/seedu/zerotoone/logic/parser/CliSyntax.java
+++ src/main/java/seedu/zerotoone/logic/parser/CliSyntax.java
@@ -11,5 +11,15 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    
+    // ZeroToOne
+    public static final Prefix PREFIX_EXERCISE_NAME = new Prefix("e/");
+    public static final Prefix PREFIX_WORKOUT_NAME = new Prefix("w/");
+    public static final Prefix PREFIX_NUM_OF_REPS = new Prefix("r/");
+    public static final Prefix PREFIX_NUM_OF_SETS = new Prefix("s/");
+    public static final Prefix PREFIX_INTERVAL = new Prefix("i/");
+    public static final Prefix PREFIX_FREQUENCY = new Prefix("f/");
+    public static final Prefix PREFIX_DATETIME = new Prefix("d/");
+
 
 }
